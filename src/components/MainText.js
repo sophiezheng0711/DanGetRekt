f7cd2a1b0dfef2e102c49affaa107b4d3396b5c5
William Joseph McCann <wmccann@fvfy833xjk7m.us.aexp.com>
2019-06-10 15:02:57 -0400

    Why was it working before uhhhh

--- src/components/MainText.js
+++ src/components/MainText.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 
 
 
-export default function LinkLabel (props) {
+export default function MainText (props) {
   return (
     <div>
       <h3>{props.text}</h3>
