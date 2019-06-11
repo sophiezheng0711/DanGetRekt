0b405e1fc608a9121b65b5dbaf87d10d7e1541b6
sophiezheng0711 <szhen4@fvfy834hjk7m.us.aexp.com>
2019-06-11 12:53:27 -0400

    fixed alignment

--- src/components/soph-puzzle.js
+++ src/components/soph-puzzle.js
@@ -1,20 +1,30 @@
 import React, {useState} from 'react';
 import Draggable from 'react-draggable';
+import {Container, Row, Col} from 'react-bootstrap';
 
 // puzzle2 is a pluggin in MainApp, therefore no navbar is needed.
 // main components needed in puzzle2: a picture hung in the background;
 // draggable scopes; a textbox prompting for the rotation of the scopes;
 // and a textbox (with button) prompting for answer.
 
+const top = {
+    position: 'absolute',
+  };
 
 class App extends React.Component {
     render() {
         return (
             <div>
-                <img src={require('./sp1.jpg')} alt='Logo' />
-                <Draggable>
-                    <div>I can now be moved around!</div>
-                </Draggable>
+                <Container>
+                    <Row>
+                        <Col><img src={require('./sp1.jpg')} alt='Logo' /></Col>
+                        <Col>
+                            <Draggable>
+                                <div style={top}>I can now be moved around!</div>
+                            </Draggable>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         );
     }
