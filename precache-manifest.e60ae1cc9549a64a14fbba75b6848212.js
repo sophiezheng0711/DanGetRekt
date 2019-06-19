5918a6569e72339ed87375f2fe954a827ce83070
sophiezheng0711 <sophie.zheng@gmail.com>
2019-06-19 16:27:25 -0400

    Updates

---
+++ precache-manifest.e60ae1cc9549a64a14fbba75b6848212.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ea6bb17f8743f3969707e6c9b0acf3",
+    "url": "/DanGetRekt/index.html"
+  },
+  {
+    "revision": "f7e97989f9b2372d507f",
+    "url": "/DanGetRekt/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "91a9206f457689fc9b04",
+    "url": "/DanGetRekt/static/js/2.5ec3ed0e.chunk.js"
+  },
+  {
+    "revision": "f7e97989f9b2372d507f",
+    "url": "/DanGetRekt/static/js/main.a39c5aac.chunk.js"
+  },
+  {
+    "revision": "eb79aa44357862088ea2",
+    "url": "/DanGetRekt/static/js/runtime~main.b94a3289.js"
+  },
+  {
+    "revision": "aeb4c53cab2b550187644af503a0f17e",
+    "url": "/DanGetRekt/static/media/black.aeb4c53c.jpg"
+  },
+  {
+    "revision": "131006b735d6fc86015a193263ac9e00",
+    "url": "/DanGetRekt/static/media/scope1.131006b7.png"
+  },
+  {
+    "revision": "db1fecb6e0df108484543fa80862474b",
+    "url": "/DanGetRekt/static/media/scope2.db1fecb6.png"
+  },
+  {
+    "revision": "b3339c883fad81ea73abc8da88f6e2da",
+    "url": "/DanGetRekt/static/media/sp1.b3339c88.png"
+  }
+]);
